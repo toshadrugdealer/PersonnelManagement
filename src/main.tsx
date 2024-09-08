168304f73ffa85ff69e00a1a32ae464235c3fc26
toshadrugdealer <t.reikh@yandex.ru>
2024-09-09 00:11:42 +0300

    q

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
 import "./styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file."
+  );
+}
