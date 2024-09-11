5654b84b9f7fd6f01f354448cdf23bcb24d9a359
toshadrugdealer <t.reikh@yandex.ru>
2024-09-12 02:51:40 +0300

    q

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
-import { FC } from "react";
-import styles from "./Header.module.scss";
+import { FC } from 'react'
+import styles from './Header.module.scss'
 
 interface HeaderProps {
-  employees: number;
-  handleModal: () => void;
+	employees: number
+	handleModal: () => void
 }
 const Header: FC<HeaderProps> = ({ employees, handleModal }) => {
-  return (
-    <header>
-      {employees ? (
-        <button className={styles.filterBtn} onClick={handleModal}></button>
-      ) : null}
-      <h1>Personnel Management</h1>
-    </header>
-  );
-};
-export default Header;
+	return (
+		<header>
+			{employees ? (
+				<button className={styles.filterBtn} onClick={handleModal}></button>
+			) : null}
+			<h1>Personnel Management</h1>
+		</header>
+	)
+}
+export default Header
